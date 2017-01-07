d090a112110747430db8ad6117ce03903016ff48
SBKubric <sbkubric@gmail.com>
2017-01-07 21:27:41 +0300

    Added argparse, ability to change top_size, days_before_today and to set access_token

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Github Trends
-
+## Description
 This script prints the 20 most starred repositories on Github over last week with their open issues number and links to them (if there any).
-
+## Usage
+Just launch it using python3:
+ ```python3 github_trending.py```
+For comlete reference launch it with -h (--help) argument:
+ ```python3 github_trending.py -h```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- github_trending.py
+++ github_trending.py
@@ -1,3 +1,4 @@
+import argparse
 import concurrent.futures
 import requests
 from datetime import date
@@ -6,36 +7,53 @@ from dateutil.relativedelta import relativedelta
 TOP_SIZE = 20
 
 
-def get_repo_open_issues_urls(full_repo_name):
-    request = requests.get('https://api.github.com/repos/%s/issues' % full_repo_name)
-    issues_urls = []
-    for issue in request.json():
-        issues_urls.append(issue['html_url'])
-    return issues_urls
+def parse_args():
+    parser = argparse.ArgumentParser(description='This script fetches the most starred repos from GitHub.')
+    parser.add_argument('-at', '--access_token', help='Use your access_token for overriding the limits of unauthorized '
+                                                      'access to GitHub API. For more info go to '
+                                                      'https://developer.github.com/v3/rate_limit/')
+    parser.add_argument('-ts', '--top_size', default=20, help='Set this value to show less/more repos. '
+                                                              'Default is 20')
+    parser.add_argument('-tp', '--time_period', default=7, help='It is amount of days during '
+                                                                'which the repos were created'
+                                                                'Default is 7')
+    return parser.parse_args()
 
 
-def get_trending_repos(top_size):
-    week_ago = date.today() + relativedelta(weeks=-1)
-    params = {'q': 'created:>%s' % week_ago,
-              'sort': 'stars',
-              'order': 'desc',
-              }
+def get_repo_open_issues_urls(full_repo_name, access_token=None):
+    params = {}
+    if access_token:
+        params['access_token'] = access_token
+        request = requests.get('https://api.github.com/repos/{}/issues'.format(full_repo_name), params=params)
+    else:
+        request = requests.get('https://api.github.com/repos/{}/issues'.format(full_repo_name))
+    return list(issue['html_url'] for issue in request.json())
+
+
+def get_trending_repos(top_size, days_before_today, access_token=None):
+    since_date = date.today() + relativedelta(days=-days_before_today)
+    params = {
+        'q': 'created:>%s' % since_date,
+        'sort': 'stars',
+        'order': 'desc',
+    }
+    if access_token:
+        params['access_token'] = access_token
     request = requests.get('https://api.github.com/search/repositories', params=params)
-    trending_repos = []
-    for repo in request.json()['items'][:top_size]:
-        trending_repos.append({
+    trending_repos = list(
+        {
             'full_name': repo['full_name'],
             'stargazers_count': repo['stargazers_count'],
-        })
+        } for repo in request.json()['items'][:top_size]
+    )
     return trending_repos
 
 
-def get_issues_count_and_issue_links(repos):
+def get_issues_count_and_issue_links(repos, access_token=None):
     pool = concurrent.futures.ThreadPoolExecutor(len(repos))
-    names = []
-    for repo in repos:
-        names.append(repo['full_name'])
-    open_issues_urls = list(pool.map(get_repo_open_issues_urls, names))
+    names = list(repo['full_name'] for repo in repos)
+    access_tokens = list(access_token for repo in repos)
+    open_issues_urls = list(pool.map(get_repo_open_issues_urls, names, access_tokens))
     for repo, repo_open_issue_urls in zip(repos, open_issues_urls):
         repo['open_issues_counter'] = len(repo_open_issue_urls)
         repo['open_issues_urls'] = repo_open_issue_urls
@@ -45,7 +63,7 @@ def get_issues_count_and_issue_links(repos):
 def print_trending_repos(repos):
     for num, repo in enumerate(repos, start=1):
         author_name, repo_name = repo['full_name'].split('/')
-        print("%s. Author: %s Repo_name: %s  Stars: %s Open_issues: %s" % (
+        print('{}., Author: {} Repo_name: {}  Stars: {} Open_issues: {}'.format(
             num,
             author_name,
             repo_name,
@@ -55,11 +73,12 @@ def print_trending_repos(repos):
         if repo['open_issues_counter']:
             print('Open issues urls:')
         for url in repo['open_issues_urls']:
-            print(' %s' % url)
+            print(' {}'.format(url))
         print('\n')
 
 
 if __name__ == '__main__':
-    trending_repos_list = get_trending_repos(TOP_SIZE)
-    get_issues_count_and_issue_links(trending_repos_list)
+    args = parse_args()
+    trending_repos_list = get_trending_repos(args.top_size, args.time_period, access_token=args.access_token)
+    get_issues_count_and_issue_links(trending_repos_list, args.access_token)
     print_trending_repos(trending_repos_list)
